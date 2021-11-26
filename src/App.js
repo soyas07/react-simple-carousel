293737181a6ab0a3eb4fe9cb306480b44a586d94
Soyas Limbu <90306699+soyas07@users.noreply.github.com>
2021-11-26 23:49:19 +1100

    Simple Carousel
    Carousel Image slide

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageData from './component/ImageData'
+import { useState } from 'react'
+import { FcNext, FcPrevious } from 'react-icons/fc'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [status, setStatus] = useState(0)
+  const length = ImageData.length
+
+  const nextSlide = () => {
+    (status === length - 1 ? setStatus(0) : setStatus(status + 1))
+  }
+
+  const prevSlide = () => {
+    (status === 0 ? setStatus(length - 1) : setStatus(status - 1))
+  }
+
+  const data = ImageData.map((item, index) => (
+    <div className={index === status ? 'slide active' : 'slide'} key={item.id}>
+      {index === status && (
+        <img 
+          src={item.image} 
+          alt="" 
+          className="image-slider"
+          id={status}
+        />
+      )}
     </div>
+  ))
+
+  return (
+    <div class="container">
+      <h1>Carousel Slide Image</h1>
+      <div className="carousel-image">
+      
+        <FcNext color="red" className="carousel-image-next-btn" onClick={nextSlide}/>
+        <FcPrevious className="carousel-image-prev-btn" onClick={prevSlide}/>
+        {data}
+      </div>
+    </div>
+
   );
 }
 
---
+++ src/component/ImageData.js
@@ -0,0 +1,22 @@
+export default ImageData = [
+    {   
+        id: 123,
+        image: "https://images.unsplash.com/photo-1637822412454-085098b4c8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 46,
+        image: "https://images.unsplash.com/photo-1637718561477-3134fe73dbdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 68,
+        image: "https://images.unsplash.com/photo-1637794261495-35d900407efc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 78,
+        image: "https://images.unsplash.com/photo-1597169628139-833e1a004acb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 12,
+        image: 'https://images.unsplash.com/photo-1637525168710-cdc43ec46797?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    }
+]
